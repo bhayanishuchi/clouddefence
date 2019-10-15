a1213f5262bb804ba1e69223e53b179be8ce775e
Shuchi <syb.freelancer@gmail.com>
2019-10-15 18:22:30 +0530

    minor fix

--- backend/app.js
+++ backend/app.js
@@ -1,21 +1,39 @@
-var bodyParser = require('body-parser')
-var express = require('express');
-var cors = require('cors');
-const UserController = require('./controllers/usercontroller');
-const CustomerController = require('./controllers/customerController');
-var mongoose = require('mongoose');
-var url = "mongodb://localhost:27017/mydb";
+const cors = require('cors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const http = require('http');
+const appConfig = require('./config/appConfig');
+const logger = require('./app/libs/loggerLib');
+const routeLoggerMiddleware = require('./app/middlewares/routeLogger.js');
+const globalErrorMiddleware = require('./app/middlewares/appErrorHandler');
+const morgan = require('morgan');
 
-mongoose.connect(url, function(err, db) {
-    if (err) throw err;
-    console.log("Database created!");
-});
+var mongoose = require('mongoose');
 
 
 var app = express();
 
+app.use(morgan('dev'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(routeLoggerMiddleware.logIp);
+app.use(globalErrorMiddleware.globalErrorHandler);
+
+
+const modelsPath = './app/models';
+const controllersPath = './app/controllers';
+const libsPath = './app/libs';
+const middlewaresPath = './app/middlewares';
+const routesPath = './app/routes';
 
 app.use(cors());
+
 app.all('*', function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -23,37 +41,92 @@ app.all('*', function (req, res, next) {
     next();
 });
 
+fs.readdirSync(modelsPath).forEach(function (file) {
+    if (~file.indexOf('.js')) require(modelsPath + '/' + file)
+});
 
-app.use(bodyParser.json())
+fs.readdirSync(routesPath).forEach(function (file) {
+    if (~file.indexOf('.js')) {
+        let route = require(routesPath + '/' + file);
+        route.setRouter(app);
+    }
+});
 
-app.post('/', [UserController.create]);
+app.use(globalErrorMiddleware.globalNotFoundHandler);
 
-app.put('/cluster/:cluster_name/stack',[UserController.updatecnoxstack]);
-app.put('/cluster/:cluster_name/monitor',[UserController.updatemonitorurl]);
-app.put('/cluster/:cluster_name/scanner',[UserController.updatescannerurl]);
-app.put('/cluster/:cluster_name/resources',[UserController.updatecount]);
-app.put('/cluster/:cluster_name/compliance',[UserController.updatecomplianceurl]);
-app.post('/logevent',[UserController.createlogevent]);
-app.get('/cluster',[UserController.findAllcluster]);
-app.get('/unseccluster',[UserController.findunseccluster]);
-app.get('/log',[UserController.findAlllogevent]);
-app.get('/totals',[UserController.findAlltotals]);
-app.get('/lists',[UserController.findAllstacklist]);
-app.delete('/deletecluster/:cluster_name',[UserController.delete]);
-app.post('/customer',[CustomerController.create]);
-app.delete('/customer',[CustomerController.delete]);
+const server = http.createServer(app);
+server.listen(appConfig.port);
+server.on('error', onError);
+server.on('listening', onListening);
+
+function onError(error) {
+    if (error.syscall !== 'listen') {
+        logger.error(error.code + ' not equal listen', 'serverOnErrorHandler', 10)
+        throw error;
+    }
 
 
+    // handle specific listen errors with friendly messages
+    switch (error.code) {
+        case 'EACCES':
+            logger.error(error.code + ':elavated privileges required', 'serverOnErrorHandler', 10);
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            logger.error(error.code + ':port is already in use.', 'serverOnErrorHandler', 10);
+            process.exit(1);
+            break;
+        default:
+            logger.error(error.code + ':some unknown error occured', 'serverOnErrorHandler', 10);
+            throw error;
+    }
+}
 
-app.listen(3001, () =>
-    console.log(`Example app listening on port 3001!`),
-);
+function onListening() {
 
-let server = require('http').createServer(app);
-server.listen(3000, () => {
+    var addr = server.address();
+    var bind = typeof addr === 'string'
+        ? 'pipe ' + addr
+        : 'port ' + addr.port;
+    ('Listening on ' + bind);
+    // logger.info('server listening on port' + addr.port, 'serverOnListeningHandler', 10);
+    let db = mongoose.connect(appConfig.db.uri, {useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true });
+    console.log('server listening on port ' + addr.port)
+}
+
+process.on('unhandledRejection', (reason, p) => {
+    console.log('Unhandled Rejection at: Promise', p, 'reason:', reason);
+    // application specific logging, throwing an error, or other logic here
+});
+
+
+mongoose.connection.on('error', function (err) {
+    console.log('database connection error');
+    console.log(err)
+    logger.error(err,
+        'mongoose connection on error handler', 10)
+    //process.exit(1)
+}); // end mongoose connection error
+
+mongoose.connection.on('open', function (err) {
+    if (err) {
+        console.log("database error");
+        console.log(err);
+        logger.error(err, 'mongoose connection open handler', 10)
+    } else {
+        console.log("database connection open success");
+        // logger.info("database connection open",
+        //     'database connection open handler', 10)
+    }
+    //process.exit(1)
+}); // enr mongoose connection o
+
+
+let newserver = require('http').createServer(app);
+newserver.listen(3000, () => {
     console.log(`socket listening on port 3000`);
 });
-let io = require('socket.io').listen(server);
+let io = require('socket.io').listen(newserver);
 global.io = io;
 
 
@@ -61,3 +134,8 @@ io.on('connection', function(socket) {
     console.log('client connected');
     app.io = io;
 });
+
+module.exports = app;
+
+
+
