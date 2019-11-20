93d1b7468a17fe209db63881ab63012ebb48d8de
Shuchi <syb.freelancer@gmail.com>
2019-11-20 23:41:00 +0530

    enviroment files

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  api : "http://34.208.221.82:3001",
-  socketUri : "http://34.208.221.82:3000",
+  api : "http://34.212.131.182:3001",
+  socketUri : "http://34.212.131.182:3000",
   // api : "http://localhost:3001",
   // socketUri : "http://localhost:3000",
 };
